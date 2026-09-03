43b177c2e8c8d1df82e8446d6a2fed56e473c83f
Pradipta Haldar <pradiptahaldar938@gmail.com>
2026-09-03 23:54:52 +0530

    another 2 approach for moving zeros

--- day4/move_zeros.py
+++ day4/move_zeros.py
@@ -10,3 +10,12 @@ class Solution(object):
         while position < len(nums):
             nums[position] = 0
             position += 1
+#another approach
+class Solution(object):
+    def moveZeroes(self, nums):
+        j = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
