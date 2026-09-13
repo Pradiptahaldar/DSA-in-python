b685ef0d695aaca91606eda6cfdc32b9bf7baaa6
Pradipta Haldar <pradiptahaldar938@gmail.com>
2026-09-13 22:43:42 +0530

    another approach for valid paranthesis:

--- day12/valid_paranthesis.py
+++ day12/valid_paranthesis.py
@@ -12,3 +12,15 @@ class Solution(object):
                 if not stack or stack.pop() != char:
                     return False
         return len(stack) == 0
+#another approach
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        pairs = {')': '(', ']': '[', '}': '{'}
+        for char in s:
+            if char in pairs:
+                if not stack or stack.pop() != pairs[char]:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
