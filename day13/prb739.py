33151f370c5fef860de1d4242f114664539bb25b
Pradipta Haldar <pradiptahaldar938@gmail.com>
2026-09-16 22:41:39 +0530

    daily temparature solved

---
+++ day13/prb739.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        result = [0] * len(temperatures)
+        stack = []
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev = stack.pop()
+                result[prev] = i - prev
+            stack.append(i)
+        return result
